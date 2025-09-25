9c7f06f456043a7b62af39fd8844374e89d8a2a4
TheSmilingParadX <118573550+TheSmilingParadX@users.noreply.github.com>
2025-09-24 17:13:07 -0700

    added tut 05

---
+++ tutorials/05-dom-javascript/js/main.js
@@ -0,0 +1,33 @@
+document.querySelector("#image-0").addEventListener("click", function() {
+    document.querySelector("#image-1").style.visibility = "visible";
+    alert("gaim");
+})
+
+document.querySelector("#image-1").addEventListener("click", function() {
+    document.querySelector("#image-2").style.visibility = "visible";
+    alert("faiz");
+})
+
+document.querySelector("#image-2").addEventListener("click", function() {
+    document.querySelector("#image-3").style.visibility = "visible";
+    alert("ryuki");
+})
+
+document.querySelector("#image-3").addEventListener("click", function() {
+    document.querySelector("#image-4").style.visibility = "visible";
+    alert("ex-aid");
+})
+
+document.querySelector("#image-4").addEventListener("click", function() {
+    document.querySelector("#image-5").style.visibility = "visible";
+    alert("zero-one");
+})
+
+document.querySelector("#image-5").addEventListener("click", function() {
+    document.querySelector("#image-6").style.visibility = "visible";
+    alert("drive");
+})
+
+document.querySelector("#image-6").addEventListener("click", function() {
+    alert("ghost");
+})
