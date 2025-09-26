16c0de657da1145fe2baba4120638f9929e6dfb6
TheSmilingParadX <118573550+TheSmilingParadX@users.noreply.github.com>
2025-09-25 21:07:04 -0700

    updated tut 05

--- tutorials/05-dom-javascript/js/main.js
+++ tutorials/05-dom-javascript/js/main.js
@@ -1,33 +1,41 @@
-document.querySelector("#image-0").addEventListener("click", function() {
-    document.querySelector("#image-1").style.visibility = "visible";
-    alert("gaim");
-})
+let img0 = document.querySelector("#image-0");
+let img1 = document.querySelector("#image-1");
+let img2 = document.querySelector("#image-2");
+let img3 = document.querySelector("#image-3");
+let img4 = document.querySelector("#image-4");
+let img5 = document.querySelector("#image-5");
+let img6 = document.querySelector("#image-6");
+let img7 = document.querySelector("#image-7");
+let img8 = document.querySelector("#image-8");
 
-document.querySelector("#image-1").addEventListener("click", function() {
-    document.querySelector("#image-2").style.visibility = "visible";
-    alert("faiz");
-})
+img0.addEventListener("click", () => {
+    img1.style.visibility = "visible";
+});
 
-document.querySelector("#image-2").addEventListener("click", function() {
-    document.querySelector("#image-3").style.visibility = "visible";
-    alert("ryuki");
-})
+img1.addEventListener("click", () => {
+    img2.style.visibility = "visible";
+});
 
-document.querySelector("#image-3").addEventListener("click", function() {
-    document.querySelector("#image-4").style.visibility = "visible";
-    alert("ex-aid");
-})
+img2.addEventListener("click", () => {
+    img3.style.visibility = "visible";
+});
 
-document.querySelector("#image-4").addEventListener("click", function() {
-    document.querySelector("#image-5").style.visibility = "visible";
-    alert("zero-one");
-})
+img3.addEventListener("click", () => {
+    img4.style.visibility = "visible";
+});
 
-document.querySelector("#image-5").addEventListener("click", function() {
-    document.querySelector("#image-6").style.visibility = "visible";
-    alert("drive");
-})
+img4.addEventListener("click", () => {
+    img5.style.visibility = "visible";
+});
 
-document.querySelector("#image-6").addEventListener("click", function() {
-    alert("ghost");
-})
+img5.addEventListener("click", () => {
+    img6.style.visibility = "visible";
+});
+
+img6.addEventListener("click", () => {
+    img7.style.visibility = "visible";
+});
+
+img7.addEventListener("click", () => {
+    img8.style.visibility = "visible";
+});
