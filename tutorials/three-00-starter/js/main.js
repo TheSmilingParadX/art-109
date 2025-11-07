cfc02ffca21ec3b6d1036f9982fcd28c2c31205f
TheSmilingParadX <118573550+TheSmilingParadX@users.noreply.github.com>
2025-11-07 15:47:08 -0800

    updated three.js tut

---
+++ tutorials/three-00-starter/js/main.js
@@ -0,0 +1,75 @@
+// Basic Three.JS scene from documentation, importing Three.JS through a CDN 
+// https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+
+
+//~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
+import * as THREE from 'three';
+
+// Import add-ons
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+// ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
+let scene, camera, renderer, cube;
+
+function init() {
+    scene = new THREE.Scene();
+    const light = new THREE.DirectionalLight(0xffffff, 3);
+    light.position.set(1, 1, 5);
+    scene.add(light);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
+    const controls = new OrbitControls(camera, renderer.domElement);
+    const loader = new GLTFLoader(); // to load 3d models
+
+    loader.load('assets/shiba.gltf', function(gltf) {
+        const shiba = gltf.scene;
+        scene.add(shiba);
+        shiba.scale.set(2, 2, 2);
+    });
+
+    // ~~~~~~~~~~~~~~~~ Create geometry ~~~~~~~~~~~~~~~~
+    const geometry = new THREE.IcosahedronGeometry();
+    // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const texture = new THREE.TextureLoader().load('textures/ice.jpg');
+    const material = new THREE.MeshStandardMaterial( { map: texture } );
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // ~~~~~~~~~~~~~~~~ Position camera ~~~~~~~~~~~~~~~~
+    camera.position.z = 5;
+}
+
+function animate() {
+    renderer.setAnimationLoop(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
+
+
+
+// ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
+// →→→→→→ Follow next steps in tutorial: 
+// // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+
+
