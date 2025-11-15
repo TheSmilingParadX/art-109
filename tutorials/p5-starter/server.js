a38b477187e283f338cf286b5c06622421fb656f
TheSmilingParadX <118573550+TheSmilingParadX@users.noreply.github.com>
2025-11-14 21:53:24 -0800

    Update server.js

--- tutorials/p5-starter/server.js
+++ tutorials/p5-starter/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'));
 
@@ -9,7 +9,7 @@ console.log("My server is running!");
 var socket = require('socket.io');
 var io = socket(server);
 
-io.sockets.on('conneciton', newConnection);
+io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
     console.log('new connection: ' + socket.id);
